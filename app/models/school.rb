c75b113e27d359275820c8098ca5f93f6fedebc3
Hannah Squier <hannahsquier@gmail.com>
2016-08-24 15:10:13 -0700

    add schools controller and show view

--- app/models/school.rb
+++ app/models/school.rb
@@ -7,4 +7,22 @@ class School < ActiveRecord::Base
   has_one :earning
   has_one :repayment
   has_one :student
+
+  has_many :bookmarks
+  has_many :users_who_bookmarked, through: :bookmarks, source: :users
+
+	def location
+		"#{school_city}, #{school_state}"
+	end
+
+	# def popular_subjects 
+	# 	attribs = academic.attributes
+	# 	attribs = attribs.delete_if { |k, v| k == "created_at" || 
+	# 														 k == "school_id" || 
+	# 														 k == "updated_at" ||
+	# 														 k == "id"
+	# 														 v.nil? }
+	# 	attribs.sort_by { |subject, percent| percent }.keys[0..3]
+	# end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :users
 
   resource :session, :only => [:new, :create, :destroy]
+  resources :schools, only: [:index, :show]
+
   get "login" => "sessions#new"
   delete "logout" => "sessions#destroy"
 
