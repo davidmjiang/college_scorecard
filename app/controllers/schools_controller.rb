32d1ed06c457a08e905d17bc4f88e2249603f1ee
Morgan Martin <morganm5201@gmail.com>
2016-08-25 17:12:54 -0400

    basic search

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,9 +2,8 @@ class SchoolsController < ApplicationController
   skip_before_action :require_login
 
   def index
-
     @query = School.index_search(query_params)
-    @schools = @query.results # TODO: add something to filter by search
+    @schools = @query # TODO: add something to filter by search
   end
 
   def show
--- app/models/school.rb
+++ app/models/school.rb
@@ -14,13 +14,6 @@ class School < ActiveRecord::Base
 
   has_many :users, through: :bookmarks
 
-
-  searchable do
-    text :school_name
-    integer :school_region_id
-    integer :school_locale
-  end
-
   def get_map_url
 		"https://maps.googleapis.com/maps/api/staticmap?center=#{urlify(school_name)},#{urlify(location)}&zoom=15&size=400x400&key=AIzaSyDTLUeLPMNZy4Gw99gQNFF6d1gyDbukKmg"
 
@@ -33,7 +26,7 @@ class School < ActiveRecord::Base
 
   def get_coords_from_location
     url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{urlify(school_name)},#{urlify(location)}&key=AIzaSyBi_LAVQdQK86p7BcCxTxYuPr1lKVC5HAw"
-     response = HTTParty.get(url, verify: false)["results"].first["geometry"]["location"]
+    response = HTTParty.get(url, verify: false)["results"].first["geometry"]["location"]
   end
 
   def urlify(location)
@@ -41,63 +34,58 @@ class School < ActiveRecord::Base
   end
 
 	def location
-		"#{school_city}, #{school_state}"
+   "#{school_city}, #{school_state}"
 	end
 
 
 	def urlify(location)
-		location.gsub(" ", "+")
+   location.gsub(" ", "+")
 	end
 
 
 	def popular_subjects
-		attribs = academic.attributes
-		attribs = attribs.delete_if { |k, v| k == "created_at" || k == "school_id" || k == "updated_at" || k == "id" }
-  	attribs = attribs.sort_by { |subject, percent| percent }.reverse[0..9].to_h
+   attribs = academic.attributes
+   attribs = attribs.delete_if { |k, v| k == "created_at" || k == "school_id" || k == "updated_at" || k == "id" }
+   attribs = attribs.sort_by { |subject, percent| percent }.reverse[0..9].to_h
    attrib_keys = attribs.map do |k, v|
-      k.gsub("program_percentage_", "")
-      k.gsub("_", " ")
-      k.titleize
-
-    end
-
-    attrib_keys.zip(attribs.values).to_h
+     k.gsub("program_percentage_", "")
+     k.gsub("_", " ")
+     k.titleize
+   end
+   attrib_keys.zip(attribs.values).to_h
 	end
 
+  def self.index_search(params)
+    school_name = params['school_name'] if params['school_name']
+    region_id = params['school_region_id'].first.to_i if params['school_region_id']
+    school_locale = params['school_locale'].first.to_i if params['school_locale']
 
-
-
-
-
-
-# Index search
-
-def self.index_search(query_params)
-  query = School.search do
-    fulltext "#{query_params[:school_name]}*"
-    any_of do
-      if query_params[:school_region_id]
-        query_params[:school_region_id].each do |region_id|
-          if query_params[:school_locale]
-            query_params[:school_locale].each do |locale|
-              all_of do
-                with(:school_region_id, region_id.to_i)
-                with(:school_locale, (locale.to_i - 2..locale.to_i))
-              end
-            end
-          else
-            with(:school_region_id, region_id.to_i)
-          end
-        end
-      elsif query_params[:school_locale]
-        query_params[:school_locale].each do |locale|
-          with(:school_locale, (locale.to_i - 2..locale.to_i))
-        end
-      end
+    query = fuzzy_search_school(school_name) if school_name
+    if query && school_locale
+      query = query.where_school_locale_equals(school_locale)
+    elsif school_locale
+      query = where_school_locale_equals(school_locale)
     end
-  end
-  query
-end
 
+    if query && region_id
+      query = query.where_region_id_equals(region_id)
+    elsif region_id
+      query = where_region_id_equals(region_id)
+    end
+    query
+  end
+
+  def self.where_school_locale_equals(school_locale)
+    where('school_locale=?', school_locale)
+  end
+
+  def self.where_region_id_equals(region_id)
+    where('school_region_id=?', region_id)
+  end
+
+  def self.fuzzy_search_school(query)
+    query = '%' + query + '%'
+    where("school_name ILIKE ?", query)
+  end
 
 end
