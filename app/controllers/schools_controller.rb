29c76361ac386b5c676b4ae42caeb53cc9697f42
Leo Ahnn <leoahnn@gmail.com>
2016-08-26 08:30:15 -0400

    sanitize order inputs

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -25,17 +25,19 @@ class SchoolsController < ApplicationController
   end
 
   def sort_by(query, options)
+    # sanitizing inputs
+    order = options[1] == 'ASC' ? 'ASC' : 'DESC'
     case options[0]
     when 'cost_in_state'
-      query.joins(:cost).order("tuition_in_state #{options[1]}")
+      query.joins(:cost).order("tuition_in_state #{order}")
     when 'cost_out_state'
-      query.joins(:cost).order("tuition_out_of_state #{options[1]}")
+      query.joins(:cost).order("tuition_out_of_state #{order}")
     when 'size'
-      query.joins(:student).order("size #{options[1]}")
+      query.joins(:student).order("size #{order}")
     when 'selectivity'
-      query.joins(:admission).order("admission_rate_overall #{options[1]}")
+      query.joins(:admission).order("admission_rate_overall #{order}")
     when 'sat_score'
-      query.joins(:admission).order("sat_scores_average_overall #{options[1]}")
+      query.joins(:admission).order("sat_scores_average_overall #{order}")
     end
   end
 end
